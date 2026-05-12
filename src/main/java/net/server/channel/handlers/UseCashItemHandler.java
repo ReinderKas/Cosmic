beee57b036777d602a1a29dafbb307e5d0ab201c
nutnnut <nutnnut@users.noreply.github.com>
2026-05-12 23:02:38 +0700

    ui reaction time

--- src/main/java/net/server/channel/handlers/UseCashItemHandler.java
+++ src/main/java/net/server/channel/handlers/UseCashItemHandler.java
@@ -622,7 +622,7 @@ public final class UseCashItemHandler extends AbstractPacketHandler {
 
                 ScrollResult scrollResult = scrolled.getLevel() > curlevel ? ScrollResult.SUCCESS : ScrollResult.FAIL;
                 player.getMap().broadcastMessage(PacketCreator.getScrollEffect(player.getId(), scrollResult, false, false));
-                BotManager.getInstance().notifyNearbyBotsOfScroll(player, scrollResult, uitem.getItemId(), 5_000L);
+                BotManager.getInstance().notifyNearbyBotsOfScroll(player, scrollResult, uitem.getItemId(), 6_000L);
                 if (eSlot < 0 && (scrollResult == ScrollResult.SUCCESS)) {
                     player.equipChanged();
                 }
